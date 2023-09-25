84ed388502498e11dab618cb4f5dabec3fcfeb75
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-25 20:25:03 +0330

    create UserRepository class and add it to ApplicationContext class

--- src/main/java/ir/maktabsharif101/hw7/repository/UserRepository.java
+++ src/main/java/ir/maktabsharif101/hw7/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package ir.maktabsharif101.hw7.repository;
 
+import java.sql.Connection;
+
 public class UserRepository {
+    private final Connection connection;
+
+    public UserRepository(Connection connection) {
+        this.connection = connection;
+    }
 
 }
--- src/main/java/ir/maktabsharif101/hw7/utility/ApplicationContext.java
+++ src/main/java/ir/maktabsharif101/hw7/utility/ApplicationContext.java
@@ -1,12 +1,15 @@
 package ir.maktabsharif101.hw7.utility;
 
 import ir.maktabsharif101.hw7.connection.JDBCConnection;
+import ir.maktabsharif101.hw7.repository.UserRepository;
 
 import java.sql.Connection;
 
 public class ApplicationContext {
     private static final Connection CONNECTION;
+    private static final UserRepository USER_REPOSITORY;
     static{
         CONNECTION= JDBCConnection.getConnection();
+        USER_REPOSITORY=new UserRepository(CONNECTION);
     }
 }
