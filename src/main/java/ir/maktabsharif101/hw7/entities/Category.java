84c212b24a06b36df139895de43cfae118c3d548
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-25 18:16:19 +0330

    create class Category

---
+++ src/main/java/ir/maktabsharif101/hw7/entities/Category.java
@@ -0,0 +1,40 @@
+package ir.maktabsharif101.hw7.entities;
+@SuppressWarnings("unused")
+public class Category {
+    Integer id;
+    String categoryName;
+    String description;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String categoryName, String description) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
