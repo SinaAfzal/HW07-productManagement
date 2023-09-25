c6f0b002532765fa1a8f3a357346dc5d5d200f50
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-25 18:47:58 +0330

    create class Brand

---
+++ src/main/java/ir/maktabsharif101/hw7/entities/Brand.java
@@ -0,0 +1,50 @@
+package ir.maktabsharif101.hw7.entities;
+@SuppressWarnings("unused")
+public class Brand {
+    private Integer id;
+    private String brandName;
+    private String website;
+    private String description;
+
+    public Brand() {
+    }
+
+    public Brand(Integer id, String brandName, String website, String description) {
+        this.id = id;
+        this.brandName = brandName;
+        this.website = website;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
