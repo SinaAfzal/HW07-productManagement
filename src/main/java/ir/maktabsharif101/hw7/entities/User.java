16ec7d53bd0c4293da2b0336201542fcfb48d122
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-25 18:13:54 +0330

    create class User

---
+++ src/main/java/ir/maktabsharif101/hw7/entities/User.java
@@ -0,0 +1,61 @@
+package ir.maktabsharif101.hw7.entities;
+@SuppressWarnings("unused")
+public class User {
+    Integer id;
+    String fullName;
+    String userName;
+    String email;
+    String password;
+
+
+    public User() {
+    }
+
+    public User(Integer id, String fullName, String userName, String email, String password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
