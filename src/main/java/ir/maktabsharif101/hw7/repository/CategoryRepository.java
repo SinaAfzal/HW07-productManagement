7159056efc9cbf683e71f90eced4dbec64595db1
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-26 10:46:39 +0330

    add save method to CategoryRepository class

--- src/main/java/ir/maktabsharif101/hw7/repository/CategoryRepository.java
+++ src/main/java/ir/maktabsharif101/hw7/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package ir.maktabsharif101.hw7.repository;
 
-import java.sql.Connection;
+import ir.maktabsharif101.hw7.entities.Category;
+
+import java.sql.*;
 
 public class CategoryRepository {
 private final Connection connection;
@@ -8,6 +10,21 @@ private final Connection connection;
     public CategoryRepository(Connection connection) {
         this.connection = connection;
     }
+
+    public Category save(Category category) throws SQLException {
+        String query="INSERT INTO category (categoryname, description) VALUES (?,?)";
+        PreparedStatement preparedStatement=connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1,category.getCategoryName());
+        preparedStatement.setString(2,category.getDescription());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet=preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        category.setId(resultSet.getInt(1));
+        return category;
+    }
+
+
+
 
 
 }
