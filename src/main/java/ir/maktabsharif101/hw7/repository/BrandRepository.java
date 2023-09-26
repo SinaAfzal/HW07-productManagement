7a4d5a5f5050c6e8b40dba2e22eded1d90431631
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-26 11:35:53 +0330

    add save method to BrandRepository class

--- src/main/java/ir/maktabsharif101/hw7/repository/BrandRepository.java
+++ src/main/java/ir/maktabsharif101/hw7/repository/BrandRepository.java
@@ -1,11 +1,26 @@
 package ir.maktabsharif101.hw7.repository;
 
-import java.sql.Connection;
+import ir.maktabsharif101.hw7.entities.Brand;
+
+import java.sql.*;
 
 public class BrandRepository {
-private final Connection connection;
+    private final Connection connection;
 
     public BrandRepository(Connection connection) {
         this.connection = connection;
+    }
+
+    public Brand save(Brand brand) throws SQLException {
+        String query="INSERT INTO brand (brandname, website, description) VALUES (?,?,?)";
+        PreparedStatement preparedStatement=connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1,brand.getBrandName());
+        preparedStatement.setString(2,brand.getWebsite());
+        preparedStatement.setString(3,brand.getDescription());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet=preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        brand.setId(resultSet.getInt(1));
+        return brand;
     }
 }
