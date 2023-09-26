6617efdde02d3eeaf594e584b2369576384fb5de
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-26 13:56:22 +0330

    add save method to ProductRepository class

--- src/main/java/ir/maktabsharif101/hw7/repository/ProductRepository.java
+++ src/main/java/ir/maktabsharif101/hw7/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package ir.maktabsharif101.hw7.repository;
 
-import java.sql.Connection;
+import ir.maktabsharif101.hw7.entities.Product;
+
+import java.sql.*;
 
 public class ProductRepository {
     private final Connection connection;
@@ -8,4 +10,17 @@ public class ProductRepository {
     public ProductRepository(Connection connection) {
         this.connection = connection;
     }
+    public Product save(Product product) throws SQLException {
+        String query="INSERT INTO product (productname, createdate, categoryid, brandid) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement=connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1,product.getProductName());
+        preparedStatement.setTimestamp(2,product.getCreateDate());
+        preparedStatement.setInt(3,product.getCategoryId());
+        preparedStatement.setInt(4,product.getBrandId());
+        ResultSet resultSet=preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        product.setId(resultSet.getInt(1));
+        return product;
+    }
+
 }
