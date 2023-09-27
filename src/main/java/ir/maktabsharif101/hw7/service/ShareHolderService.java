76d6ff493663fc52a41f08d1d0aedc8c89acef6c
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 17:47:59 +0330

    add save method to ShareHolderService class

--- src/main/java/ir/maktabsharif101/hw7/service/ShareHolderService.java
+++ src/main/java/ir/maktabsharif101/hw7/service/ShareHolderService.java
@@ -1,6 +1,9 @@
 package ir.maktabsharif101.hw7.service;
 
+import ir.maktabsharif101.hw7.entities.ShareHolder;
 import ir.maktabsharif101.hw7.repository.ShareHolderRepository;
+
+import java.sql.SQLException;
 
 public class ShareHolderService {
     private final ShareHolderRepository shareHolderRepository;
@@ -8,4 +11,21 @@ public class ShareHolderService {
     public ShareHolderService(ShareHolderRepository shareHolderRepository) {
         this.shareHolderRepository = shareHolderRepository;
     }
+
+    public ShareHolder save(ShareHolder shareHolder) throws SQLException {
+        if (shareHolderRepository.doesExist(shareHolder.getNationalCode())) {
+            System.out.println("The shareholder already exists on the database!");
+            return null;
+        } else {
+            ShareHolder returnedShareholder = shareHolderRepository.save(shareHolder);
+            if (returnedShareholder != null) {
+                System.out.println("ID=" + returnedShareholder.getId() + " is assigned to the shareholder and it is stored on database successfully!");
+                return returnedShareholder;
+            } else {
+                System.out.println("OOPS! Something went wrong!");
+                return null;
+            }
+        }
+    }
+
 }
