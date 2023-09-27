f748728d17688e0477fc6a953b42e08025001877
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-28 03:16:19 +0330

    call menu

--- src/main/java/ir/maktabsharif101/hw7/HW7Application.java
+++ src/main/java/ir/maktabsharif101/hw7/HW7Application.java
@@ -1,7 +1,16 @@
 package ir.maktabsharif101.hw7;
 
+
+import ir.maktabsharif101.hw7.menu.Menu;
+import ir.maktabsharif101.hw7.utility.ApplicationContext;
+
+import java.sql.SQLException;
+
 public class HW7Application {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
+        Menu menu=ApplicationContext.getMenu();
+        menu.homePage();
 
     }
 }
