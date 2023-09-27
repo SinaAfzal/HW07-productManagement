5085783703bfddc819e1dfd0bc11da08eccf55e7
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 18:37:43 +0330

    add save method to UserService class

--- src/main/java/ir/maktabsharif101/hw7/service/UserService.java
+++ src/main/java/ir/maktabsharif101/hw7/service/UserService.java
@@ -1,6 +1,9 @@
 package ir.maktabsharif101.hw7.service;
 
+import ir.maktabsharif101.hw7.entities.User;
 import ir.maktabsharif101.hw7.repository.UserRepository;
+
+import java.sql.SQLException;
 
 public class UserService {
     private final UserRepository userRepository;
@@ -8,4 +11,22 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public User save(User user) throws SQLException {
+        if (userRepository.doesExist(user.getUserName()) || userRepository.mailDoesExist(user.getEmail())) {
+            System.out.println("username or email already exist on database!");
+            return null;
+        } else {
+            User returnedUser = userRepository.save(user);
+            if (returnedUser != null) {
+                System.out.println("ID=" + returnedUser.getId() + " is assigned to the user and it is stored on the database!");
+                return returnedUser;
+            } else {
+                System.out.println("OOPS! Something went wrong!");
+                return null;
+            }
+        }
+    }
+
+
 }
