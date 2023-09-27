9b8a8f06f3f6c4e2eda634f1f3f2e537ea1d194d
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 21:18:05 +0330

    add isEmailValid method to Validation class

--- src/main/java/ir/maktabsharif101/hw7/utility/Validation.java
+++ src/main/java/ir/maktabsharif101/hw7/utility/Validation.java
@@ -6,9 +6,17 @@ import java.util.regex.Pattern;
 public class Validation {
     public static boolean isPasswordValid(String password){
         Pattern pattern =
-                Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
+                Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");//at least one upper-case, one lower-case and one digit and at least 8 characters
         Matcher matcher=pattern.matcher(password);
         return matcher.matches();
     }
+
+    public static boolean isEmailValid(String email){
+        Pattern pattern=Pattern.compile("^(.+)@(\\S+)$"); //check the presence of @ .
+        Matcher matcher=pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
 
 }
