408ca44c8a774268cd8ef95a72bda731caca5b8c
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 21:12:53 +0330

    add isPasswordValid method to Validation class

--- src/main/java/ir/maktabsharif101/hw7/utility/Validation.java
+++ src/main/java/ir/maktabsharif101/hw7/utility/Validation.java
@@ -1,5 +1,14 @@
 package ir.maktabsharif101.hw7.utility;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validation {
-    
+    public static boolean isPasswordValid(String password){
+        Pattern pattern =
+                Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
+        Matcher matcher=pattern.matcher(password);
+        return matcher.matches();
+    }
+
 }
