99577507a8884f52c765fa3ee310f04c20b5da4a
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 09:26:52 +0330

    update save method of Brand class to include the shareholder-brand relationship

--- src/main/java/ir/maktabsharif101/hw7/repository/BrandRepository.java
+++ src/main/java/ir/maktabsharif101/hw7/repository/BrandRepository.java
@@ -12,41 +12,50 @@ public class BrandRepository {
     }
 
     public Brand save(Brand brand) throws SQLException {
-        String query="INSERT INTO brand (brandname, website, description) VALUES (?,?,?)";
-        PreparedStatement preparedStatement=connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1,brand.getBrandName());
-        preparedStatement.setString(2,brand.getWebsite());
-        preparedStatement.setString(3,brand.getDescription());
+        String brandTableQuery = "INSERT INTO brand (brandname, website, description) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(brandTableQuery, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, brand.getBrandName());
+        preparedStatement.setString(2, brand.getWebsite());
+        preparedStatement.setString(3, brand.getDescription());
         preparedStatement.executeUpdate();
-        ResultSet resultSet=preparedStatement.getGeneratedKeys();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
         resultSet.next();
         brand.setId(resultSet.getInt(1));
+
+        String shareholder_brand_TableQuery = "INSERT INTO shareholder_brand (brandid, shareholderid) VALUES (?,?)";
+        PreparedStatement preparedStatement_brand_shareholder = connection.prepareStatement(shareholder_brand_TableQuery);
+        for (int i = 0; i < brand.getShareHolderIds().length; i++) {
+            preparedStatement_brand_shareholder.setInt(1, brand.getId());
+            preparedStatement_brand_shareholder.setInt(2, brand.getShareHolderIds()[i]);
+            preparedStatement_brand_shareholder.addBatch();
+        }
+        preparedStatement_brand_shareholder.executeBatch();
         return brand;
     }
 
     public boolean doesExist(Brand brand) throws SQLException {
-        String query="SELECT brandname FROM brand WHERE brandname=?";
-        PreparedStatement preparedStatement=connection.prepareStatement(query);
-        preparedStatement.setString(1,brand.getBrandName());
-        ResultSet resultSet=preparedStatement.executeQuery();
+        String query = "SELECT brandname FROM brand WHERE brandname=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, brand.getBrandName());
+        ResultSet resultSet = preparedStatement.executeQuery();
         return resultSet.next();
     }
 
-    public int updateBrandName(String newBrandName,int id) throws SQLException{
-        String query="UPDATE brand SET brandname=? WHERE id=?";
-        PreparedStatement preparedStatement=connection.prepareStatement(query);
-        preparedStatement.setString(1,newBrandName);
-        preparedStatement.setInt(2,id);
-        int result=preparedStatement.executeUpdate();
+    public int updateBrandName(String newBrandName, int id) throws SQLException {
+        String query = "UPDATE brand SET brandname=? WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, newBrandName);
+        preparedStatement.setInt(2, id);
+        int result = preparedStatement.executeUpdate();
         return result;
     }
 
-    public int updateBrandDescription(String newBrandDescription,int id) throws SQLException{
-        String query="UPDATE brand SET description=? WHERE id=?";
-        PreparedStatement preparedStatement=connection.prepareStatement(query);
-        preparedStatement.setString(1,newBrandDescription);
-        preparedStatement.setInt(2,id);
-        int result=preparedStatement.executeUpdate();
+    public int updateBrandDescription(String newBrandDescription, int id) throws SQLException {
+        String query = "UPDATE brand SET description=? WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, newBrandDescription);
+        preparedStatement.setInt(2, id);
+        int result = preparedStatement.executeUpdate();
         return result;
     }
 
