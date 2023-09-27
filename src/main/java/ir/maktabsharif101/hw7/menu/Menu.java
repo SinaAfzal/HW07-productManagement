323b693d96d5de4c3ebdd068dd524d48f085afb5
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 20:56:55 +0330

    add homePage method to Menu class

--- src/main/java/ir/maktabsharif101/hw7/menu/Menu.java
+++ src/main/java/ir/maktabsharif101/hw7/menu/Menu.java
@@ -2,6 +2,10 @@ package ir.maktabsharif101.hw7.menu;
 
 import ir.maktabsharif101.hw7.service.*;
 
+import javax.sound.midi.SysexMessage;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Menu {
     private final BrandService brandService;
     private final CategoryService categoryService;
@@ -20,5 +24,40 @@ public class Menu {
         this.userService = userService;
         this.shareHolderService = shareHolderService;
     }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void homePage() throws SQLException {
+        boolean homePageIsActive = true;
+        while (homePageIsActive) {
+            System.out.println("+--------------------------------------------------+");
+            System.out.println("|                HomeWork7-Maktab101               |");
+            System.out.println("|      Product/Brand/Category Management system    |");
+            System.out.println("|           Developed by: Sina Afzalsoltani        |");
+            System.out.println("+--------------------------------------------------+");
+            System.out.println();
+            System.out.println();
+            System.out.println("Choose an option from the menu:");
+            System.out.println("1- Sign Up");
+            System.out.println("2- Sign in");
+            System.out.println("3- Exit");
+
+            String input=scanner.next();
+
+            switch (input){
+                case "1"-> register();
+                case "2"-> login();
+                case "3"-> homePageIsActive=false;
+                default -> {
+                    System.out.println("Invalid input!");
+                    scanner.next();
+                    System.out.println("**********  Press any key to continue   **********");
+                }
+            }
+        }
+    }
+
+
+
 
 }
