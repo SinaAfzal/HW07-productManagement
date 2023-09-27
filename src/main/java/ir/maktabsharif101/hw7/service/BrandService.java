950c219a266648a16e0f7b08772a421689ef0d44
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-27 14:28:10 +0330

    add save method to BrandService class

--- src/main/java/ir/maktabsharif101/hw7/service/BrandService.java
+++ src/main/java/ir/maktabsharif101/hw7/service/BrandService.java
@@ -1,6 +1,9 @@
 package ir.maktabsharif101.hw7.service;
 
+import ir.maktabsharif101.hw7.entities.Brand;
 import ir.maktabsharif101.hw7.repository.BrandRepository;
+
+import java.sql.SQLException;
 
 public class BrandService {
     private final BrandRepository brandRepository;
@@ -8,4 +11,22 @@ public class BrandService {
     public BrandService(BrandRepository brandRepository) {
         this.brandRepository = brandRepository;
     }
+
+    public Brand save(Brand brand) throws SQLException {
+        if (brandRepository.doesExist(brand)) {
+            System.out.println("The brand name already exists on the database!");
+            return null;
+        } else {
+            Brand returnedBrand = brandRepository.save(brand);
+            if (returnedBrand != null) {
+                System.out.println("ID=" + returnedBrand.getId() + " is assigned to the brand and it is stored on the database!");
+                return returnedBrand;
+            } else {
+                System.out.println("OOPS! Something went wrong!");
+                return null;
+            }
+        }
+    }
+
+
 }
